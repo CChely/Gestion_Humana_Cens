c14a4b9923f1bcc52b1df86b36b87dcb86d2586c
Leonardo Burgos Diaz <leburgosdiaz@outlook.com>
2026-05-11 17:16:59 -0500

    Feature: opcion de configuracion de seguridad en el menu

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -143,6 +143,13 @@ export const defaultNavigation: FuseNavigationItem[] = [
                 link: '/ajustes/configuracion-perfil'
             },
             {
+                id: 'ajustes.configuracion-seguridad',
+                title: 'Configuración de seguridad',
+                type: 'basic',
+                icon: 'heroicons_outline:shield-check',
+                link: '/ajustes/configuracion-seguridad'
+            },
+            {
                 id: 'ajustes.empresa',
                 title: 'Empresa',
                 type: 'basic',
@@ -446,7 +453,7 @@ export const horizontalNavigation: FuseNavigationItem[] = [
         icon: 'heroicons_outline:user-group',
         children: []
     },
-     {
+    {
         id: 'carga-masiva',
         title: 'Carga-masiva',
         type: 'basic',
@@ -454,7 +461,7 @@ export const horizontalNavigation: FuseNavigationItem[] = [
         link: '/carga-masiva',
         children: []
     },
-     {
+    {
         id: 'alertas',
         title: 'Alertas',
         type: 'basic',
